94425177a11391ab35de48f78ae26edfac9b8764
antonioadv08 <antonioadv08@gmail.com>
2023-06-06 21:49:14 +0200

    change header

--- components/cryptotable/cryptoTableHeader.js
+++ components/cryptotable/cryptoTableHeader.js
@@ -1,25 +1,40 @@
-"use client"
-import ChevronDown from "../../public/svg/chevronDown"
+"use client";
 
 const styles = {
-    textIcon: `flex items-center`
-}
+  textIcon: `flex items-center`,
+};
 
 const cryptoTableHeader = () => {
-    return <tbody>
-        <tr>
-            <th></th>
-            <th className="flex items-center"><b># &nbsp;</b><ChevronDown /></th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>24h %</th>
-            <th>7d %</th>
-            <th><div className={styles.textIcon}><p className="mr-2">Market Cap</p></div></th>
-            <th><div className={styles.textIcon}><p className="mr-2">Volume(24h)</p></div></th>
-            <th><div className={styles.textIcon}><p className="mr-2">Circulating Supply</p></div></th>
-            <th>Last 7 days</th>
-        </tr>
+  return (
+    <tbody>
+      <tr>
+        <th></th>
+        <th className="flex items-center">
+          <b># &nbsp;</b>
+        </th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>24h %</th>
+        <th>7d %</th>
+        <th>
+          <div className={styles.textIcon}>
+            <p className="mr-2">Market Cap</p>
+          </div>
+        </th>
+        <th>
+          <div className={styles.textIcon}>
+            <p className="mr-2">Volume(24h)</p>
+          </div>
+        </th>
+        <th>
+          <div className={styles.textIcon}>
+            <p className="mr-2">Circulating Supply</p>
+          </div>
+        </th>
+        <th>Last 7 days</th>
+      </tr>
     </tbody>
-}
+  );
+};
 
-export default cryptoTableHeader
+export default cryptoTableHeader;
